708a9df2679f6b3ed8a8d28744f86312cd7a2f23
Travis Czerw <travis.czerw@gmail.com>
2015-01-26 11:47:59 -0600

    1st Function Done

---
+++ app/scripts/main.js
@@ -0,0 +1,34 @@
+/* jshint devel:true */
+$(document).ready(onReady);
+
+function onReady() {
+
+	$.get(
+		'http://www.omdbapi.com/?s=postman',
+		onResultsReceived,
+		'json'
+		);
+
+	function onResultsReceived(data) {
+		console.log(data);
+	}
+
+	function onResults(data) {
+		$('#resultBox').val(data.Search[0].Title)
+	}
+
+	function searchMovie(query) {
+		$.get(
+		'http://www.omdbapi.com/',
+		{s: query},
+		onResults,
+		'json'
+		);
+	}
+
+	$('.myButton').click(function(event) {
+		$('#resultBox').val($('#searchBox').val())
+		//searchMovie($('#searchBox').val());
+	});
+}	
+
